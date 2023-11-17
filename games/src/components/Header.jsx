0dfb38630840d6a177d6070782f1df3bf0c798b5
Edmig1 <124930647+Edmig1@users.noreply.github.com>
2023-11-17 10:50:42 -0300

    header começado

---
+++ games/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import s from './Header.module.css'
+import {Link} from "react-router-dom";
+function Header(){
+    return(
+        <header className={s.header}>
+            <h1>RAGE</h1>
+            <nav>
+                <Link className={s.link} to={''}>Home</Link>
+                <Link className={s.link} to={''}>Promoções</Link>
+                <Link className={s.link} to={''}>Desejos</Link>
+            </nav>
+        </header>
+    )
+}
+export default Header
---
+++ games/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home(){
+    return(
+        <Header></Header>
+    )
+
+}
+export default Home
