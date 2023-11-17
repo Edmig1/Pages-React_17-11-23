7d21a1b246a02106c7d1c2874d399875e4c7a5bd
nicolasdosantos <104533715+nicolasdosantos@users.noreply.github.com>
2023-11-17 13:47:54 -0300

    Descrição

--- games/src/App.js
+++ games/src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./components/Header";
 import ListaDesejo from "./components/ListaDesejo";
+import Descricao from "./pages/Descricao";
 
 function App() {
   return (
       <BrowserRouter>
           <Header></Header>
         <Routes>
-
+            <Route path="/jogo/:jogo" element={<Descricao />} />
         </Routes>
           <ListaDesejo img='/The_Last_of_Us_capa 1.svg'></ListaDesejo>
+
       </BrowserRouter>
   );
 }
