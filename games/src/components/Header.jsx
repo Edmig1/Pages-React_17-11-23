712f2535684bcfb23162632ff7c215181a2f2dda
Edmig1 <124930647+Edmig1@users.noreply.github.com>
2023-11-17 14:51:07 -0300

    sem os ponto

--- games/src/components/Header.jsx
+++ games/src/components/Header.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 function Header(){
     return(
         <header className={s.header}>
-            <img src="./logo.png" style={{width:'6%'}} alt=""/>
+            <img src="/logo.png" style={{width:'6%'}} alt=""/>
             <nav>
                 <Link className={s.link} to={''}>Home</Link>
                 <Link className={s.link} to={''}>Promoções</Link>
--- games/src/pages/Home.jsx
+++ games/src/pages/Home.jsx
@@ -5,9 +5,9 @@ import CardHome from "../components/CardHome";
 function Home(){
     return(
         <div className={s.divtotal}>
-            <CardHome  img='./ea.webp' nome={'EA FC'} texto='R$249.99' x='88%'></CardHome>
-            <CardHome  img='./brawl.jpg' nome={'Brawlhalla'} texto='GRÁTIS' x='50%'>></CardHome>
-            <CardHome  img='./last.png' nome={'The Last Of Us'} texto='R$99.99' x='50%'>></CardHome>
+            <CardHome  img='/ea.webp' nome={'EA FC'} texto='R$249.99' x='88%'></CardHome>
+            <CardHome  img='/brawl.jpg' nome={'Brawlhalla'} texto='GRÁTIS' x='50%'>></CardHome>
+            <CardHome  img='/last.png' nome={'The Last Of Us'} texto='R$99.99' x='50%'>></CardHome>
         </div>
     )
 
